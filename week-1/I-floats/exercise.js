138366dab362d783b4c232bd2b0911d257ce18ec
Ed <edhedcw@gmail.com>
2018-03-23 18:05:43 +0000

    w1Iex1

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,11 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+
+var total = numberOfMentors + numberOfStudents;
+var centst = numberOfStudents / total * 100;
+var rs = Math.round(centst);
+var centmen = numberOfMentors / total * 100;
+var rm = Math.round(centmen);
+
+console.log("Percentage students: " + rs + "%");
+console.log("Percentage mentors: " + rm + "%");
