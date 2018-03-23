618fd1b1fdfa17eaa2db19eda16655d927886c5c
Ed <edhedcw@gmail.com>
2018-03-23 17:16:33 +0000

    done G ex1

--- week-1/G-strings-methods/exercise.js
+++ week-1/G-strings-methods/exercise.js
@@ -1,3 +1,10 @@
-// Start by creating a variable `message`
+var intro = "Yo, my tag be ";
+var name = "Ed";
+
+var contu = ". and me tag be ";
+var howbig = name.length;
+var outro = " characters long, mate.";
+
+var message = intro + name + contu + howbig + outro;
 
 console.log(message);
