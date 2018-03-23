1e359d3e89967e1ed867e200654ef4fc2daaa7c8
Ed <edhedcw@gmail.com>
2018-03-23 18:14:52 +0000

    w1Jex2

--- week-1/J-functions/exercise2.js
+++ week-1/J-functions/exercise2.js
@@ -1,7 +1,12 @@
 function triple(number) {
   // complete function here
+  return number * 3
 }
 
-var result = triple(12);
+var result = triple(8);
+var seven = triple(7);
+var nine = triple(9);
 
 console.log(result);
+console.log(seven);
+console.log(nine);
