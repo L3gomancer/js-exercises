cccd897a52fc4470172085551ca6f7d59f332265
Ed <edhedcw@gmail.com>
2018-03-23 17:16:43 +0000

    done G ex2

--- week-1/G-strings-methods/exercise2.js
+++ week-1/G-strings-methods/exercise2.js
@@ -1,3 +1,11 @@
+var intro = "Yo, my tag be ";
+// name cemented in exercise
 var name = " Daniel  ";
+var trimmed = name.trim();
+var num = trimmed.length;
+var contu = ". and me tag be ";
+var outro = " characters long, mate.";
+
+var message = intro + trimmed + contu + num + outro;
 
 console.log(message);
