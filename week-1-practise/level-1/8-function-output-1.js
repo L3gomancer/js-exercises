e2a04b536ff22e648ebd3f619fd5787b183681ce
Ed <edhedcw@gmail.com>
2018-04-14 00:22:46 +0100

    p1l1ex8

--- week-1-practise/level-1/8-function-output-1.js
+++ week-1-practise/level-1/8-function-output-1.js
@@ -7,6 +7,11 @@ console.log(result);
 
 // Answer these questions:
 // - What does this program do?
+// generate a random number
 // - How many parameters does the function take?
+// none just now
 // - What is the function name?
+// getNumber
 // - Where is the function called? with what parameters?
+// line 5
+// with no parameters
